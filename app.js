250250fe460c1555b35e9edd48c0d7dee7d6bdbf
DOANTHIBANGTRINH <trinhb1906789@student.ctu.edu.vn>
2022-03-06 22:20:41 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError, errorHandler } = require("./app/errors");
 const app = express();
 
 app.use(cors());
@@ -8,6 +9,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contack book application." });
+});
+
+setupContactRoutes(app);
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    errorHandler.handleError(error, res);
 });
 
 module.express = app;
